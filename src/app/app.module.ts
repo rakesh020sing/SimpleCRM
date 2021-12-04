624b383b4a8ec3cc04c7bf61f33e25193bb6ac25
Rakesh Sing <rakesh020sing@gmail.com>
2021-12-04 16:39:18 +0530

    angular project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,31 @@
+import { ProfileModule } from './profile/profile.module';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule } from '@angular/material/button';
+import { AddContactComponent } from './contacts/add-contact/add-contact.component';
+import { EditContactComponent } from './contacts/edit-contact/edit-contact.component';
+import { ListContactComponent } from './contacts/list-contact/list-contact.component';
+import { DeleteContactComponent } from './contacts/delete-contact/delete-contact.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddContactComponent,
+    EditContactComponent,
+    ListContactComponent,
+    DeleteContactComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    ProfileModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/contacts/add-contact/add-contact.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnChanges, OnDestroy, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-add-contact',
+  templateUrl: './add-contact.component.html',
+  styleUrls: ['./add-contact.component.scss'],
+})
+export class AddContactComponent implements OnInit, OnChanges, OnDestroy {
+  constructor() {}
+
+  ngOnInit(): void {}
+  ngOnChanges() {}
+  ngOnDestroy() {}
+}
